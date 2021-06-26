14dd40fafef4d93daf067a52ce15eea55afb061b
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-26 13:04:46 +0200

    created a POST API endpoint to save our data

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json({ limit: '50mb'}));
+
+app.post('/api/stores', (req, res) => {
+  let dbStores = req.body;
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  
 })
 
 app.listen(3000, () => {
