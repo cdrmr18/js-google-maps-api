4e4b80e4a008343e40ce931bd8286bdd99a0524b
Kathy Tavia <kathy.tavia@gmail.com>
2021-06-26 20:42:34 +0200

    Setup Mongoose to connect to MongoDB

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+
+mongoose.connect('mongodb+srv://ktavia:@cluster0.bstqd.mongodb.net/GoogleMapsApp?retryWrites=true&w=majority', {useNewUrlParser: true}, 
+{ useUnifiedTopology: true });
 
 app.use(express.json({ limit: '50mb'}));
 
 app.post('/api/stores', (req, res) => {
   let dbStores = req.body;
+  res.send('sent post');
 })
 
 app.get('/', (req, res) => {
-  
+  res.send('sent get');
 })
 
 app.listen(3000, () => {
